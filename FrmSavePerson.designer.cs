25c2abdd137415a8e76b487a0c288411f4c46c63
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-04-13 22:25:54 -0300

    recolocando a coluna id na tela salvePerson invisible

--- FrmSavePerson.designer.cs
+++ FrmSavePerson.designer.cs
@@ -114,7 +114,7 @@
             // 
             this.ndNumberOfMembers.Location = new System.Drawing.Point(318, 219);
             this.ndNumberOfMembers.Name = "ndNumberOfMembers";
-            this.ndNumberOfMembers.Size = new System.Drawing.Size(127, 26);
+            this.ndNumberOfMembers.Size = new System.Drawing.Size(127, 30);
             this.ndNumberOfMembers.TabIndex = 8;
             this.ndNumberOfMembers.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.ndNumberOfMembers.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ndNumberOfMembers_KeyPress);
@@ -126,7 +126,7 @@
             this.label9.Location = new System.Drawing.Point(315, 195);
             this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(151, 20);
+            this.label9.Size = new System.Drawing.Size(188, 25);
             this.label9.TabIndex = 35;
             this.label9.Text = "Composição familiar";
             // 
@@ -136,7 +136,7 @@
             this.txtHelp.Margin = new System.Windows.Forms.Padding(4);
             this.txtHelp.MaxLength = 100;
             this.txtHelp.Name = "txtHelp";
-            this.txtHelp.Size = new System.Drawing.Size(143, 26);
+            this.txtHelp.Size = new System.Drawing.Size(143, 30);
             this.txtHelp.TabIndex = 7;
             this.txtHelp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHelp_KeyPress);
             this.txtHelp.Leave += new System.EventHandler(this.txtHelp_Leave);
@@ -148,7 +148,7 @@
             this.label8.Location = new System.Drawing.Point(164, 195);
             this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(103, 20);
+            this.label8.Size = new System.Drawing.Size(128, 25);
             this.label8.TabIndex = 33;
             this.label8.Text = "Bolsa Família";
             // 
@@ -158,7 +158,7 @@
             this.txtIncome.Margin = new System.Windows.Forms.Padding(4);
             this.txtIncome.MaxLength = 100;
             this.txtIncome.Name = "txtIncome";
-            this.txtIncome.Size = new System.Drawing.Size(143, 26);
+            this.txtIncome.Size = new System.Drawing.Size(143, 30);
             this.txtIncome.TabIndex = 6;
             this.txtIncome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIncome_KeyPress);
             this.txtIncome.Leave += new System.EventHandler(this.txtIncome_Leave);
@@ -170,7 +170,7 @@
             this.label7.Location = new System.Drawing.Point(13, 195);
             this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.Size = new System.Drawing.Size(69, 25);
             this.label7.TabIndex = 31;
             this.label7.Text = "Renda";
             // 
@@ -179,7 +179,7 @@
             this.mkPhone.Location = new System.Drawing.Point(313, 103);
             this.mkPhone.Mask = "(00) 0 0000-0000";
             this.mkPhone.Name = "mkPhone";
-            this.mkPhone.Size = new System.Drawing.Size(132, 26);
+            this.mkPhone.Size = new System.Drawing.Size(132, 30);
             this.mkPhone.TabIndex = 3;
             // 
             // label6
@@ -189,7 +189,7 @@
             this.label6.Location = new System.Drawing.Point(309, 79);
             this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.Size = new System.Drawing.Size(76, 25);
             this.label6.TabIndex = 29;
             this.label6.Text = "Tel/Cel";
             // 
@@ -199,7 +199,7 @@
             this.txtNumberAddress.Margin = new System.Windows.Forms.Padding(4);
             this.txtNumberAddress.MaxLength = 100;
             this.txtNumberAddress.Name = "txtNumberAddress";
-            this.txtNumberAddress.Size = new System.Drawing.Size(143, 26);
+            this.txtNumberAddress.Size = new System.Drawing.Size(143, 30);
             this.txtNumberAddress.TabIndex = 5;
             // 
             // label5
@@ -209,7 +209,7 @@
             this.label5.Location = new System.Drawing.Point(298, 132);
             this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.Size = new System.Drawing.Size(81, 25);
             this.label5.TabIndex = 27;
             this.label5.Text = "Número";
             // 
@@ -219,7 +219,7 @@
             this.txtAddress.Margin = new System.Windows.Forms.Padding(4);
             this.txtAddress.MaxLength = 200;
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(277, 26);
+            this.txtAddress.Size = new System.Drawing.Size(277, 30);
             this.txtAddress.TabIndex = 4;
             // 
             // label4
@@ -229,7 +229,7 @@
             this.label4.Location = new System.Drawing.Point(18, 132);
             this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(172, 20);
+            this.label4.Size = new System.Drawing.Size(214, 25);
             this.label4.TabIndex = 26;
             this.label4.Text = "Endereço/Comunidade";
             // 
@@ -239,7 +239,7 @@
             this.txtRG.Margin = new System.Windows.Forms.Padding(4);
             this.txtRG.MaxLength = 14;
             this.txtRG.Name = "txtRG";
-            this.txtRG.Size = new System.Drawing.Size(155, 26);
+            this.txtRG.Size = new System.Drawing.Size(155, 30);
             this.txtRG.TabIndex = 2;
             // 
             // label3
@@ -249,7 +249,7 @@
             this.label3.Location = new System.Drawing.Point(145, 79);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.Size = new System.Drawing.Size(40, 25);
             this.label3.TabIndex = 24;
             this.label3.Text = "RG";
             // 
@@ -258,7 +258,7 @@
             this.mkCPF.Location = new System.Drawing.Point(17, 103);
             this.mkCPF.Mask = "000,000,000-00";
             this.mkCPF.Name = "mkCPF";
-            this.mkCPF.Size = new System.Drawing.Size(120, 26);
+            this.mkCPF.Size = new System.Drawing.Size(120, 30);
             this.mkCPF.TabIndex = 1;
             // 
             // label2
@@ -268,7 +268,7 @@
             this.label2.Location = new System.Drawing.Point(13, 79);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.Size = new System.Drawing.Size(52, 25);
             this.label2.TabIndex = 21;
             this.label2.Text = "CPF";
             // 
@@ -278,7 +278,7 @@
             this.txtName.Margin = new System.Windows.Forms.Padding(4);
             this.txtName.MaxLength = 200;
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(428, 26);
+            this.txtName.Size = new System.Drawing.Size(428, 30);
             this.txtName.TabIndex = 0;
             this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
             // 
@@ -289,7 +289,7 @@
             this.label1.Location = new System.Drawing.Point(18, 24);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(64, 25);
             this.label1.TabIndex = 19;
             this.label1.Text = "Nome";
             // 
@@ -346,7 +346,7 @@
             this.dtDateBenefits.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtDateBenefits.Location = new System.Drawing.Point(22, 156);
             this.dtDateBenefits.Name = "dtDateBenefits";
-            this.dtDateBenefits.Size = new System.Drawing.Size(103, 26);
+            this.dtDateBenefits.Size = new System.Drawing.Size(103, 30);
             this.dtDateBenefits.TabIndex = 10;
             // 
             // label11
@@ -357,7 +357,7 @@
             this.label11.Location = new System.Drawing.Point(18, 132);
             this.label11.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(158, 20);
+            this.label11.Size = new System.Drawing.Size(192, 25);
             this.label11.TabIndex = 33;
             this.label11.Text = "Data do recebimento";
             // 
@@ -368,7 +368,7 @@
             this.label10.Location = new System.Drawing.Point(18, 22);
             this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(80, 20);
+            this.label10.Size = new System.Drawing.Size(99, 25);
             this.label10.TabIndex = 32;
             this.label10.Text = "Descrição";
             // 
@@ -417,6 +417,7 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dgvBenefitsReceived.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvBenefitsReceived.RowHeadersVisible = false;
+            this.dgvBenefitsReceived.RowHeadersWidth = 51;
             this.dgvBenefitsReceived.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvBenefitsReceived.Size = new System.Drawing.Size(395, 399);
             this.dgvBenefitsReceived.TabIndex = 5;
@@ -426,14 +427,18 @@
             // ColumnID
             // 
             this.ColumnID.HeaderText = "ID";
+            this.ColumnID.MinimumWidth = 6;
             this.ColumnID.Name = "ColumnID";
             this.ColumnID.ReadOnly = true;
             this.ColumnID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ColumnID.Visible = false;
+            this.ColumnID.Width = 125;
             // 
             // Colescripition
             // 
             this.Colescripition.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Colescripition.HeaderText = "Descrição";
+            this.Colescripition.MinimumWidth = 6;
             this.Colescripition.Name = "Colescripition";
             this.Colescripition.ReadOnly = true;
             this.Colescripition.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
@@ -442,14 +447,15 @@
             // 
             this.ColDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.ColDate.HeaderText = "Data do recebimento";
+            this.ColDate.MinimumWidth = 6;
             this.ColDate.Name = "ColDate";
             this.ColDate.ReadOnly = true;
             this.ColDate.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.ColDate.Width = 164;
+            this.ColDate.Width = 198;
             // 
             // FrmSavePerson
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(900, 484);
